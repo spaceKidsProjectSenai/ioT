ce9d7d8a9ccb169e4f7e3fc95da5baa5d2702587
mazevedofs <marina.mafs@gmail.com>
2018-04-05 21:18:20 -0300

    testes

--- teste.js
+++ teste.js
@@ -1,61 +1,97 @@
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
 var board = new five.Board({
-  io: new Raspi()
+    io: new Raspi()
 });
 
 
 board.on("ready", function () {
 
-  // Create a new `button` hardware instance.
-  var buttonGreen = new five.Button("P1-16");
-  var buttonBlue = new five.Button("P1-7");
-
-  var ledGreen = new five.Led("P1-13");
-  var ledBlue = new five.Led("P1-11");
-
-  var passo = 5;
-
-  var resultNumber = init();
-
-  buttonGreen.on("down", function () {
-      console.log("Button pressed");
-      if (resultNumber === 1) {
-          resultNumber = ledOff(ledGreen, passo);
-          passo--;
-      }
-  });
+    // Create a new `button` hardware instance.
+    var button1 = new five.Button("P1-16");
+    var button2 = new five.Button("P1-7");
+    var button3 = new five.Button("P1-16");
+    var button4 = new five.Button("P1-7");
+    var button5 = new five.Button("P1-16");
+    var button6 = new five.Button("P1-7");
+    var button7 = new five.Button("P1-16");
+    var button8 = new five.Button("P1-7");
+    var button9 = new five.Button("P1-16");
+    var button10 = new five.Button("P1-7");
+    var button11 = new five.Button("P1-16");
+    var button12 = new five.Button("P1-7");
 
 
-  buttonBlue.on("down", function () {
-      console.log("Button pressed");
-      if (resultNumber === 0) {
-          resultNumber = ledOff(ledBlue, passo);
-          passo--;
-      }
-  });
 
-  function ledOff(led, passo) {
-      led.off();
-      if (passo != 0) {
-          return init();
-      } else {
-          console.log("Acabooooou!")
-      }
-  }
-  
-  function init() {
-  
-      let random = Math.random();
-      let number = Math.round(random);
-  
-      console.log(random);
-      if (number === 1) {
-          ledGreen.on();
-      } else {
-          ledBlue.on();
-      }
-      return number;
-  }
+    var led1 = new five.Led("P1-13");
+    var led2 = new five.Led("P1-11");
+    var led3 = new five.Led("P1-13");
+    var led4 = new five.Led("P1-11");
+    var led5 = new five.Led("P1-13");
+    var led6 = new five.Led("P1-11");
+    var led7 = new five.Led("P1-13");
+    var led8 = new five.Led("P1-11");
+    var led9 = new five.Led("P1-13");
+    var led10 = new five.Led("P1-11");
+    var led11 = new five.Led("P1-13");
+    var led12 = new five.Led("P1-11");
+    
+    var acerto = 0;
+    var erro = 0;
+
+    var passo = 12;
+
+    var resultNumber = init();
+
+    button1.on("down", function () {
+        console.log("Button pressed");
+        if (resultNumber === 1) {
+            resultNumber = ledOff(led1, passo);
+            passo--;
+            acerto++;
+            console.log(acerto);
+        }else{
+            erro++;
+            console.log(erro);
+        }
+        
+    });
+
+
+    button2.on("down", function () {
+        console.log("Button pressed");
+        if (resultNumber === 0) {
+            resultNumber = ledOff(led2, passo);
+            passo--;
+            acerto++;
+            console.log(acerto);
+        }else{
+            erro++;
+            console.log(erro);
+        }
+    });
+
+    function ledOff(led, passo) {
+        led.off();
+        if (passo != 0) {
+            return init();
+        } else {
+            console.log("Acabooooou!")
+        }
+    }
+
+    function init() {
+
+        let random = Math.random();
+        let number = Math.round(random);
+
+        console.log(random);
+        if (number === 1) {
+            led1.on();
+        } else {
+            led2.on();
+        }
+        return number;
+    }
 
 });
